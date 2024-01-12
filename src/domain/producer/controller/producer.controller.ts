c9ffdf86702611ab8df515ad9c017776c7e9b19b
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2024-01-12 10:48:31 -0300

    feat: create producer controller

---
+++ src/domain/producer/controller/producer.controller.ts
@@ -0,0 +1,65 @@
+import { DeleteResult } from "typeorm";
+import { BadRequestError, InternalServerError, NotFoundError } from "../../../helpers/ApiErrors";
+import ProducerDeletedDto from "../dto/producer-deleted.dto";
+import ProducerDto from "../dto/producer.dto";
+import Producer from "../entity/producer.entity";
+import ProducerRepositoryInterface from "../repository/producer.repository.interface";
+import { Response, Request } from "express";
+import HttpStatus from 'http-status-codes';
+export default class ProducerController {
+
+  constructor(private readonly producerRepository: ProducerRepositoryInterface) {
+    this.createProducer = this.createProducer.bind(this);
+    this.getAll = this.getAll.bind(this);
+    this.getById = this.getById.bind(this);
+    this.delete = this.delete.bind(this);
+  }
+
+  async createProducer(request: Request, response: Response): Promise<unknown> {
+    try {
+      const producerDto: ProducerDto = new ProducerDto(request.body)
+      await producerDto.validate();
+      const producer = new Producer(producerDto.name, producerDto.document)
+      const producerStored = await this.producerRepository.create(producer);
+      return response.status(HttpStatus.CREATED).json(producerStored)
+    } catch (e: any) {
+      console.log(e.toString());
+      throw new BadRequestError(e)
+    }
+  }
+
+  async getAll(request: Request, response: Response): Promise<unknown> {
+    try {
+      const allProducers = await this.producerRepository.findAll()
+      return response.status(HttpStatus.OK).json(allProducers)
+    } catch (e: any) {
+      console.log(e.toString());
+      throw new InternalServerError(e.toString())
+    }
+  }
+
+  async getById(request: Request, response: Response): Promise<any> {
+    const producerId = request.params.id;
+    const producerStored = await this.producerRepository.findById(producerId);
+    if (!producerStored) {
+      throw new NotFoundError('Producer not found')
+    }
+    return response.status(HttpStatus.OK).json(producerStored)
+  }
+
+  async delete(request: Request, response: Response): Promise<any> {
+    const producerId = request.params.id
+    const producerStored = await this.producerRepository.findById(producerId);
+    if (!producerStored) {
+      throw new NotFoundError('Producer not found')
+    }
+    const result: DeleteResult = await this.producerRepository.delete(producerId)
+    const affected = result.affected;
+    let deleted = false;
+    if (affected) {
+      deleted = true ? affected.valueOf() > 0 : false
+    }
+    const deletedDto = new ProducerDeletedDto(producerId, deleted)
+    return response.status(HttpStatus.OK).json(deletedDto);
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import ProducerController from "./domain/producer/controller/producer.controller";
+import { AppDataSource } from "./infrastructure/database/typeorm/postgres/data-source";
+import { ProducerRepository } from "./infrastructure/database/repository/producer.repository";
+import { getDataSource } from "./infrastructure/database/config";
 
 const routes = Router();
+const producerController = new ProducerController(new ProducerRepository(getDataSource()))
+routes.post('/api/producers', producerController.createProducer)
+routes.get('/api/producers', producerController.getAll)
+routes.get('/api/producers/:id', producerController.getById)
+routes.delete('/api/producers/:id', producerController.delete)
 
 export default routes;

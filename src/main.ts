c901fd3b60be88eb8fddf92c82976702d2ee0707
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2024-01-12 10:54:36 -0300

    feat: update main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
+import 'express-async-errors'
 import express from 'express'
 import routes from './routes';
 import { AppDataSource } from './infrastructure/database/typeorm/postgres/data-source';
+import { httpError } from './middlewares/http-errors';
 
 const port = 3000;
 
@@ -9,6 +11,7 @@ AppDataSource.initialize()
     const app = express();
     app.use(express.json())
     app.use(routes)
+    app.use(httpError)
     return app.listen(port, () => {
       console.log(`App listening on port ${port}`)
     })

e727e0460fde9faa321ec936c711cce0d3112a03
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2024-01-13 06:47:30 -0300

    tests: create tests to create farm route

--- src/domain/farm/controller/farm.e2e.spec.ts
+++ src/domain/farm/controller/farm.e2e.spec.ts
@@ -6,6 +6,8 @@ import { httpError } from '../../../middlewares/http-errors';
 import { AppDataSourceTest } from '../../../infrastructure/database/typeorm/postgres/data-source-test';
 import HttpStatus from 'http-status-codes'
 import FarmEntity from '../../../infrastructure/database/typeorm/entities/farms.entity';
+import ProducerEntity from '../../../infrastructure/database/typeorm/entities/producer.entity';
+import Producer from '../../producer/entity/producer.entity';
 
 describe('Farms routes tests', () => {
 
@@ -14,26 +16,138 @@ describe('Farms routes tests', () => {
   app.use(httpError)
   app.use(routes)
 
+  let farmRepository: any
+  let producerRepository: any
+
+  const producer = new Producer(
+    "Name",
+    "292.256.890-39"
+  )
+
+  let requestBodyStubValid: any;
+  let producerStored: any;
+
   beforeAll(async () => {
     await AppDataSourceTest.initialize();
-    // const repository = AppDataSourceTest.getRepository(FarmEntity);
-    // const producerStored = await repository.save(producerStub);
-    // validId = producerStored.id
+    farmRepository = AppDataSourceTest.getRepository(FarmEntity);
+    producerRepository = AppDataSourceTest.getRepository(ProducerEntity);
+    await farmRepository.delete({})
+    await producerRepository.delete({})
+
+    producerStored = await producerRepository.save(producer)
+    requestBodyStubValid = {
+      name: 'Farm name',
+      city: 'City',
+      state: 'State',
+      producerId: producerStored.id,
+      totalArea: 10,
+      arableArea: 2,
+      vegetationArea: 4,
+      crops: [
+        'cotton',
+        'coffe'
+      ]
+    }
   })
 
   afterAll(async () => {
     const repository = AppDataSourceTest.getRepository(FarmEntity);
-    await repository.clear();
+    await repository.delete({});
     await AppDataSourceTest.close();
   })
 
   describe('Create', () => {
-    it('should return a new Producer', async () => {
-      const newProducer = {
-        name: 'Producer Test',
-        document: '292.256.890-39'
+    const repository = AppDataSourceTest.getRepository(ProducerEntity);
+
+    it('should return a new Farm with valid data', async () => {
+      const farmStubValid = {
+        name: 'Farm name',
+        city: 'City',
+        state: 'State',
+        producerId: producerStored.id,
+        totalArea: 10,
+        arableArea: 2,
+        vegetationArea: 4,
+        crops: [
+          'cotton',
+          'coffe'
+        ]
       }
-      const response = await supertest(app).post('/api/farms').send(newProducer).expect(HttpStatus.OK);
+      const response = await supertest(app).post('/api/farms').send(farmStubValid).expect(HttpStatus.CREATED);
+    })
+
+    it('should return an error with invalid producerId', async () => {
+      const farmStubValid = {
+        name: 'Farm name',
+        city: 'City',
+        state: 'State',
+        producerId: 'b1419ac5-0d1b-416f-b6d7-9fcdb12a8dc4',
+      }
+      const response = await supertest(app).post('/api/farms').send(farmStubValid).expect(HttpStatus.BAD_REQUEST);
+    })
+
+    it('should return an error with invalid name', async () => {
+      const farmStubValid = {
+        name: '',
+        city: 'City',
+        state: 'State',
+        producerId: producerStored.id,
+      }
+      const response = await supertest(app).post('/api/farms').send(farmStubValid).expect(HttpStatus.BAD_REQUEST);
+    })
+
+    it('should return an error with invalid city', async () => {
+      const farmStubValid = {
+        name: 'Name',
+        city: '',
+        state: 'State',
+        producerId: producerStored.id,
+      }
+      const response = await supertest(app).post('/api/farms').send(farmStubValid).expect(HttpStatus.BAD_REQUEST);
+    })
+
+    it('should return an error with invalid state', async () => {
+      const farmStubValid = {
+        name: 'Name',
+        city: 'City',
+        state: '',
+        producerId: producerStored.id,
+      }
+      const response = await supertest(app).post('/api/farms').send(farmStubValid).expect(HttpStatus.BAD_REQUEST);
+    })
+
+    it('should return an error with invalid area proportion', async () => {
+      const farmStubValid = {
+        name: 'Farm name',
+        city: 'City',
+        state: 'State',
+        producerId: producerStored.id,
+        totalArea: 10,
+        arableArea: 7,
+        vegetationArea: 4,
+        crops: [
+          'cotton',
+          'coffe'
+        ]
+      }
+      const response = await supertest(app).post('/api/farms').send(farmStubValid).expect(HttpStatus.BAD_REQUEST);
+    })
+
+    it('should return an error with invalid crop', async () => {
+      const farmStubValid = {
+        name: 'Farm name',
+        city: 'City',
+        state: 'State',
+        producerId: producerStored.id,
+        totalArea: 10,
+        arableArea: 3,
+        vegetationArea: 4,
+        crops: [
+          'rice',
+          'coffe'
+        ]
+      }
+      const response = await supertest(app).post('/api/farms').send(farmStubValid).expect(HttpStatus.BAD_REQUEST);
     })
   })
 }) 

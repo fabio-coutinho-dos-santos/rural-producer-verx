89192a0d5c0e757acdbf17557149f420351c7242
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2024-01-20 10:39:15 -0300

    feat: update cors policies

--- src/main.ts
+++ src/main.ts
@@ -4,22 +4,23 @@ import { AppDataSource } from "./infrastructure/database/typeorm/postgres/dataso
 import swaggerUI from "swagger-ui-express";
 import * as YAML from "yamljs";
 import { httpError } from "./infrastructure/api/middlewares/http-errors";
-import cors from "cors";
 import { API_CONFIG } from "./infrastructure/api/config";
 import logger from "./infrastructure/logger/pino.logger";
 import producerRoutes from "./infrastructure/api/resources/producer/producer.routes";
 import farmRoutes from "./infrastructure/api/resources/farm/farm.routes";
+import { corsOptions } from "./infrastructure/api/helpers/cors-options";
+import cors from 'cors'
 
 const port = API_CONFIG.port;
 
 const swaggerDocument = YAML.load("./docs/api.yaml");
 AppDataSource.initialize().then(() => {
   const app = express();
-  app.use(cors({ origin: "*" }));
   app.use(express.json());
   app.use(producerRoutes);
   app.use(farmRoutes);
   app.use(httpError);
+  app.use(cors(corsOptions))
   app.use(
     `/api/${API_CONFIG.version}/doc`,
     swaggerUI.serve,
